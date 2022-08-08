1341fc5aee52dd3d0c23723e067c64a5292c4942
drako9159 <chochito9159@gmail.com>
2022-08-08 11:56:11 -0500

    new project emogi for search emogis

---
+++ src/components/emogiPicker/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    symbol: "😀",
+    name: "cara alegre",
+    keywords: "smiling face happy feliz alegre",
+  },
+  {
+    symbol: "❤",
+    name: "corazon",
+    keywords: "corazon heart love like",
+  },
+  {
+    symbol: "😡",
+    name: "cara furiosa",
+    keywords: "enojo enojado furioso",
+  },
+  {
+    symbol: "🔥",
+    name: "fuego",
+    keywords: "fire hot burn fuego quema",
+  },
+  {
+    symbol: "📕",
+    name: "book",
+    keywords: "book libro read",
+  },
+  {
+    symbol: "💻",
+    name: "computer",
+    keywords: "computer computadora",
+  },
+];
